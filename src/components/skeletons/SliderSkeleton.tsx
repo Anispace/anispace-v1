70cf8b4b0ced7fac91a7264b6bf7bf8e519a1e89
apurv <102051299+apurvsikka@users.noreply.github.com>
2024-04-03 21:51:49 +0530

    Update SliderSkeleton.tsx

--- src/components/skeletons/SliderSkeleton.tsx
+++ src/components/skeletons/SliderSkeleton.tsx
@@ -4,7 +4,7 @@ type SliderSkeletonProps = {
 
 export default function SliderSkeleton({ error }: SliderSkeletonProps) {
   return (
-    <div className={` bg-skeleton w-full h-[40vh] sm:h-[60dvh] grid place-content-center ${!error && 'animate-pulse'}`}>
+    <div className={`rounded bg-skeleton w-full h-[40vh] sm:h-[60dvh] grid place-content-center ${!error && 'animate-pulse'}`}>
       {error instanceof Error ? (
         <h4 className="text-textColor bg-backgroundColor/20 px-3 py-4 rounded-md">
           {error.message}
